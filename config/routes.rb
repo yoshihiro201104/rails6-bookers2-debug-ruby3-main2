b624333a086129d74d8006c9fc4471cd609ffe71
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-30 06:55:38 +0000

    [Add]kadai-2

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   # resources :users, only: [:index,:show,:edit,:update]
+  # resources :users, only: [:show]
+  resource :users
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
